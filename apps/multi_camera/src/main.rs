81df8bf13f0658481d9734db8a66c52e6d20d50f
w4ngzhen <w4ngzhen@hotmail.com>
2024-09-25 09:38:04 +0800

    modify content.

--- apps/multi_camera/src/main.rs
+++ apps/multi_camera/src/main.rs
@@ -12,9 +12,23 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // add camera.
     commands.spawn(Camera2dBundle::default());
     // then spawn Sprite.
-    let icon: Handle<Image> = asset_server.load("icon.png");
+    let img1: Handle<Image> = asset_server.load("img1.png");
     commands.spawn(SpriteBundle {
-        texture: icon.clone(),
+        texture: img1.clone(),
+        transform: Transform {
+            translation: Vec3 {
+                z: 1.,
+                x: 100.,
+                ..default()
+            },
+            ..default()
+        },
+        ..default()
+    });
+    // add another Sprite with img2.png
+    let img2: Handle<Image> = asset_server.load("img2.png");
+    commands.spawn(SpriteBundle {
+        texture: img2.clone(),
         ..default()
     });
 }
